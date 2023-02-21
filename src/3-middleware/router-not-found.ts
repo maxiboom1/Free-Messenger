63d760010a4d734111175ffae7b4b09d1175e3c7
Alex Samih-zade <galutziogen@gmail.com>
2023-02-21 22:39:35 +0200

    reorder

--- src/3-middleware/router-not-found.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { RouteNotFoundError } from "../2-models/client-errors";
-
-function routeNotFound(request: Request, response: Response, next: NextFunction) {
-    const err = new RouteNotFoundError(request.originalUrl);
-    next(err);
-}
-
-export default routeNotFound;
