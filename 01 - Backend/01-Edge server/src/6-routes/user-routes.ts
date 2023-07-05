182413c414a30cf9d915cde106628a366184aae2
maxiboom1 <galutziogen@gmail.com>
2023-07-05 15:06:43 +0300

    comment

--- 01 - Backend/01-Edge server/src/6-routes/user-routes.ts
+++ 01 - Backend/01-Edge server/src/6-routes/user-routes.ts
@@ -5,10 +5,10 @@ const router = express.Router();
 const UserServiceProxy = require('express-http-proxy');
 
 
-// GET http://localhost:4000/profileImage/:imageNmae
+// GET http://localhost:4000/profileImage/:imageName and route request to users-service
 router.get("/profileImage/:profileImageName", async (request: Request, response: Response, next: NextFunction) => {
     
-    const getProfileImageProxy = UserServiceProxy(appConfig.profileImageUrl); // Route to internal 
+    const getProfileImageProxy = UserServiceProxy(appConfig.profileImageUrl); 
     getProfileImageProxy(request, response, next);
     
 });
